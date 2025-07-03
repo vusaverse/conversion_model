e294e36f1bf267dbf398a57e0579a1356a91a744
Timo Koster <t.s.koster@vu.nl>
2025-07-03 18:22:25 +0200

    feat: wait for october

--- models/Conversion/main.R
+++ models/Conversion/main.R
@@ -1,5 +1,5 @@
 testdatum <- today()
-nTest_year = vvconverter::academic_year(testdatum) + 1
+nTest_year = vvconverter::academic_year(testdatum + months(1)) + 1
 
 source("models/Conversion/00. Prepare aanmeldingen.R")
 source("models/Conversion/01. Conversion prediction.R")
